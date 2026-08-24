f6be548ad605700816aac3e2c1534879179a97c2
abghilbgm <hil-blg.ipfiot@adityabirla.com>
2026-08-24 15:54:10 +0530

    NOTRE

--- powerbi/refresh_powerbi_data.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d %~dp0\..
-call .venv\Scripts\activate.bat
-python powerbi\build_powerbi_dataset.py --host localhost --port 5432 --db ppe_compliance --user ppe_user --password ppe_password --out powerbi\powerbi_data
